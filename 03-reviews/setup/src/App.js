8b5abb3b0d52d77f749a39c494f49cf419668748
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-19 15:45:18 +0530

    03 Reviews structure complete

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -3,11 +3,13 @@ import Review from "./Review";
 function App() {
   return (
     <main>
-      <div className="container">
+      <section className="container">
         <div className="title">
           <h2>our reviews</h2>
+          <div className="underline"></div>
         </div>
-      </div>
+        <Review />
+      </section>
     </main>
   );
 }
