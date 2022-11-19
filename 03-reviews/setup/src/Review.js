162580a15081dc9152597826f617d06925f9a9f4
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-19 16:18:19 +0530

    previous, next, surprize button functionality added

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -5,7 +5,36 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
-  console.log(people);
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
   return (
     <article className="review">
       <div className="img-container">
@@ -19,13 +48,15 @@ const Review = () => {
       <p className="info">{text}</p>
       <div className="button-container">
         <button className="prev-btn">
-          <FaChevronLeft />
+          <FaChevronLeft onClick={prevPerson} />
         </button>
         <button className="next-btn">
-          <FaChevronRight />
+          <FaChevronRight onClick={nextPerson} />
         </button>
       </div>
-      <button className="random-btn">surprize me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprize me
+      </button>
     </article>
   );
 };
